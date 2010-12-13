c448fb136086dbbab2478d5a727ad4bea4310f4d
Leon Rosenberg <leon.rosenberg@anotheria.net>
2010-12-13 00:13:23 +0000

    re-added

---
+++ java/net/anotheria/anoplass/api/APIConfigurable.java
@@ -0,0 +1,43 @@
+package net.anotheria.anoplass.api;
+
+import org.configureme.annotations.Configure;
+import org.configureme.annotations.ConfigureMe;
+
+/**
+ * ConfigureME based Config. APIConfigurable - for API configuring.
+ */
+@ConfigureMe(name = "apiconfig")
+public class APIConfigurable {
+	/**
+	 * APIConfigurable 'verboseMethodCalls'.
+	 */
+	@Configure private boolean verboseMethodCalls;
+
+	/**
+	 * If false the sessions won't be tied to http sessions.
+	 */
+	@Configure private boolean associateSessions = false;  
+	
+	/**
+	 * Constructor.
+	 */
+	public APIConfigurable(){
+	}
+
+	public boolean isVerboseMethodCalls() {
+		return verboseMethodCalls;
+	}
+
+	public void setVerboseMethodCalls(boolean value) {
+		verboseMethodCalls = value;
+	}
+
+	public boolean isAssociateSessions() {
+		return associateSessions;
+	}
+
+	public void setAssociateSessions(boolean associateSessions) {
+		this.associateSessions = associateSessions;
+	}
+
+}
